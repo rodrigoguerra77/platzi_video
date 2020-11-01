474ac4f98d6efcd7a75e52803aae737805f18aee
Rodrigo Guerra <rodrigoguerra77@gmail.com>
2020-10-31 21:43:35 -0400

    Use a fake api for list in the app.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -7,42 +7,53 @@ import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
+const App = () => {
+  const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
 
-    <Categories title='Mi Lista'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+  useEffect(() => {
+    fetch('http://localhost:3000/initalState')
+      .then(response => response.json())
+      .then(data => setVideos(data));
+  }, []);
 
-    <Categories title='Tendencias'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
 
-    <Categories title='Originales de Platzi Video'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+      {videos.mylist.length > 0 &&
+        <Categories title='Mi Lista'>
+          <Carousel>
+            {videos.mylist.map(item =>
+              <CarouselItem key={item.id} {...item} />
+            )}
+          </Carousel>
+        </Categories>
+      }
+      
+      {videos.trends.length > 0 &&
+        <Categories title='Tendencias'>
+          <Carousel>
+            {videos.trends.map(item =>
+              <CarouselItem key={item.id} {...item} />
+            )}
+          </Carousel>
+        </Categories>
+      }
 
-    <Footer />
-  </div>
-);
+      {videos.originals.length > 0 &&
+        <Categories title='Originales de Platzi Video'>
+          <Carousel>
+            {videos.originals.map(item =>
+              <CarouselItem key={item.id} {...item} />
+            )}
+          </Carousel>
+        </Categories>
+      }
+
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
